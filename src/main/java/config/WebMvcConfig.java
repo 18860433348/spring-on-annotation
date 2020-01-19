9461a6e7d52ace3f0a23c23e265edd3eadb51ebb
chenchuan <18860433348@163.com>
2020-01-19 11:42:42 +0800

    代码调整

--- src/main/java/config/WebMvcConfig.java
+++ src/main/java/config/WebMvcConfig.java
@@ -1,19 +1,10 @@
 package config;
 
-import com.alibaba.fastjson.serializer.SerializerFeature;
-import com.alibaba.fastjson.support.config.FastJsonConfig;
-import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
-import org.springframework.context.annotation.Bean;
+
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.MediaType;
-import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
 *@Author DoubleC
